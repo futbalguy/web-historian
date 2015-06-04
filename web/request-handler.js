3f70b8226049764d85644da72beaf0035a612cf0
Hack Reactor Students <accounts@hackreactor.com>
2015-06-03 20:22:15 -0700

    passing some tests

--- web/request-handler.js
+++ web/request-handler.js
@@ -6,7 +6,7 @@
 //   res.end(archive.paths.list);
 // };
 
-
+var url = require('url')
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var httpHelper = require('./http-helpers.js');
@@ -18,29 +18,75 @@ var indexPagePath = '/Users/HR10/2015-05-web-historian/web/public/index.html'
 
 exports.handleRequest = function (req, res) {
 
-var headers = httpHelper.headers;
+  var headers = httpHelper.headers;
 
-if (req.method === 'GET') {
-  var statusCode = 200;
-  res.writeHead(statusCode, headers);
-} else if (req.method === 'POST') {
-  var statusCode = 201;
-
-} else if (req.method === 'OPTIONS') {
-  var statusCode = 200;
-}
+  var parsedURL = url.parse(req.url, true);
+  var pathName = parsedURL.pathname;
+  var site = pathName.slice(1)
 
 
-  var filePath = indexPagePath;
-  console.log(filePath)
-  var responseData;
-  fs.readFile(filePath,'utf8',function(err,data) {
-   res.end(data);
-  });
+  if (req.method === 'GET') {
+    //header
+    var statusCode = 200;
+    res.writeHead(statusCode, headers);
+
+    //body
+    if (!site) {
+      var filePath = archive.paths.siteAssets + '/index.html';
+      console.log(filePath)
+      fs.readFile(filePath,'utf8',function(err,data) {
+       res.end(data);
+      });
+    } else {
+
+      var siteLoc = archive.paths.archivedSites + '/' + site;
+
+      fs.readFile(siteLoc,'utf8',function(err,siteHTML) {
+        res.end(siteHTML);
+      });
+    }
+
+  } else if (req.method === 'POST') {
+    //header
+    var statusCode = 201;
+    res.writeHead(statusCode, headers);
+
+    //console.log('req: ',req)
+
+    var postData = '';
+    req.on('data', function(item){
+      postData += item;
+    });
+
+    req.on('end', function(){
+      //var jsonData = JSON.parse(postData)
+      //unescape(jsonData);
+     // jsonData.roomname = _.unescape(roomName);
+      console.log(postData);
+    })
+
+    //body
+  } else if (req.method === 'OPTIONS') {
+    var statusCode = 200;
+    res.writeHead(statusCode, headers);
+  }
 
 
 
-    // console.dir(archive.paths.list)
+
+
 
 
 };
+
+
+    // var sitesPath = archive.paths.list;
+    // console.log('sites path: ' + sitesPath);
+    // fs.readFile(sitesPath,'utf8',function(err,sitesData) {
+    //  // res.end(sitesData);
+    //   var sitesArray = sitesData.split('\n');
+    //   console.log(site);
+    //   console.log('sites array: ', sitesArray);
+    //   if (sitesArray.indexOf(site) !== -1) {
+    //   }
+    // });
